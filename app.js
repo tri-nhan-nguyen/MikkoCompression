e5a2d09801e2bcb5262998648b831014223ae6e2
Nhan Nguyen <nhan.nguyen@uwaterloo.ca>
2018-06-22 00:02:00 -0400

    Added remote image OCR

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 var tesseract = require('node-tesseract'),
-	express = require("express"),
+    request = require('request'),
+    express = require('express'),
+    fs = require('fs');
 	app = express();
-const url = "public/images/example.jpeg";
+const url = "https://upload.wikimedia.org/wikipedia/commons/7/75/Southern_Life_in_Southern_Literature_text_page_322.jpg";
+const fileName = "text.png";
+
+var writeFileStream = fs.createWriteStream(fileName);
 
 // App config
 app.set("view engine", "ejs");
@@ -9,13 +14,17 @@ app.use(express.static(__dirname + "/public"));
 
 // Render homepage
 app.get("/", function(req, res) {
-	tesseract.process(url, (err, text) => {
-        if(err){
-            return console.log("An error occured: ", err);
-        }
-        console.log("Recognized text:");
-        console.log(text);
-    });
+    request(url).pipe(writeFileStream).on('close', function() {
+        console.log(url, 'saved to', fileName);
+        tesseract.process(fileName, (err, text) => {
+            if (err) {
+                console.log("An error occured: ", err);
+                return;
+            }
+            console.log("Recognized text:");
+            console.log(text);
+        });
+    })
 	res.render("index");
 });
 
